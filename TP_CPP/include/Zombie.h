d9df9764604db447be34728db0c7cd800c4c9512
anatole-P <72392172+anatole-P@users.noreply.github.com>
2021-02-08 17:00:24 +0100

    Add files via upload

---
+++ TP_CPP/include/Zombie.h
@@ -0,0 +1,19 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+
+class Zombie
+{
+    public:
+        //CONSTRUCTEUR
+        Zombie();
+
+
+        //DESTRUCTEUR
+        ~Zombie();
+    protected:
+
+    private:
+};
+
+#endif // ZOMBIE_H
